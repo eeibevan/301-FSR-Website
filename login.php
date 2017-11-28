52ab0523399169ddbb62c48a8b463e634cb8d98f
Evan Black <me@evanpblack.com>
2017-11-27 21:36:56 -0500

    Remove Validation On Login Fields

--- login.php
+++ login.php
@@ -7,6 +7,7 @@
     <title>Log In</title>
 
     <?php require_once 'scriptAndCss.php' ?>
+
     <style>
         #signInForm input {
             margin-top: 15px;
@@ -19,27 +20,26 @@
                 var form = $("#signInForm");
                 form.submit(function () {
                     event.preventDefault();
-                    var htmlForm = form[0];
 
                     var email = $("#inputEmail").val();
                     var pass = $("#inputPassword").val();
 
-                    if (htmlForm.checkValidity()) {
-                        $.ajax({
-                            type: "POST",
-                            url: './index.php?path=/api/login',
-                            dataType: 'json',
-                            data: {
-                                username: email,
-                                password: pass
-                            }
-                        }).done(function () {
-                            window.location.replace('./index.php?path=/home');
-                        }).fail(function (jqXHR, textStatus, errorThrown) {
-                            var data = jqXHR.responseJSON;
+                    $.ajax({
+                        type: "POST",
+                        url: './index.php?path=/api/login',
+                        dataType: 'json',
+                        data: {
+                            username: email,
+                            password: pass
+                        }
+                    }).done(function (data, status, xhr) {
+                        window.location.replace('./index.php?path=/home');
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                        var data = jqXHR.responseJSON;
+                        if (data !== undefined)
                             $("#signInError").show().html(data.message);
-                        })
-                    }
+                    })
+
                 })
             })
         })();
@@ -49,11 +49,11 @@
 
 <?php require_once 'nav.php' ?>
 
-<main role="main" class="container">
-    <form id="signInForm" class="form-signin offset-4 col-4" >
+<main role="main" class="container col-lg-4">
+    <form id="signInForm" class="form-signin" novalidate>
         <h2 class="form-signin-heading">Please sign in</h2>
         <div id="signInError" class="alert alert-danger" role="alert" style="display: none"></div>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <button id="btnLogin" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
